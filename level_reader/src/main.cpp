6d1453a64ee4e2cd15853900e082438a1a23921b
Carl Philipp Reh <carlphilippreh@gmail.com>
2016-02-13 16:00:47 +0100

    Use alda to output a level

--- level_reader/src/main.cpp
+++ level_reader/src/main.cpp
@@ -1,290 +1,56 @@
-#include <fcppt/io/read.hpp>
-#include <fcppt/either/apply.hpp>
-#include <fcppt/algorithm/generate_n.hpp>
-#include <fcppt/container/raw_vector.hpp>
-#include <fcppt/optional/apply.hpp>
-#include <fcppt/optional/object.hpp>
-#include <fcppt/optional/bind.hpp>
-#include <fcppt/either/object.hpp>
-#include <fcppt/either/map.hpp>
-#include <fcppt/either/cat.hpp>
-#include <fcppt/either/bind.hpp>
-#include <fcppt/either/from_optional.hpp>
-#include <fcppt/either/apply.hpp>
-#include <fcppt/make_strong_typedef.hpp>
+#include <libstryker/level/read.hpp>
+#include <libstryker/level/record.hpp>
+#include <alda/raw/record_output.hpp>
+#include <majutsu/record_output.hpp>
+#include <fcppt/math/vector/output.hpp>
+#include <fcppt/optional/maybe.hpp>
 #include <fcppt/config/external_begin.hpp>
-#include <cstdint>
-#include <cstring>
-#include <algorithm>
-#include <istream>
-#include <vector>
-#include <array>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
 #include <fcppt/config/external_end.hpp>
 
-namespace
+namespace std
 {
-FCPPT_MAKE_STRONG_TYPEDEF(
-  std::string,
-  error_string);
-
-template<std::streamsize N>
-fcppt::either::object<error_string,std::string>
-read_string(std::istream &s,std::string const &_error)
+template<typename T, std::size_t N>
+std::ostream &operator<<(std::ostream &_stream,std::array<T,N> const &_array)
 {
-  char chars[N];
-  return
-    s.read(chars,N)
-    ?
-      fcppt::either::object<error_string,std::string>{std::string(chars,std::find(chars,chars+N,0))}
-    :
-      fcppt::either::object<error_string,std::string>{error_string{_error}};
+  _stream << '(';
+  for(T const &val : _array)
+    _stream << val << ',';
+  return _stream << ')';
 }
-
 template<typename T>
-fcppt::either::object<error_string,fcppt::container::raw_vector<T>>
-read_array(
-  std::istream &s,
-  std::size_t const n,
-  fcppt::endianness::format const _format,
-  error_string const &_error)
+std::ostream &operator<<(std::ostream &_stream,std::vector<T> const &_array)
 {
-  fcppt::container::raw_vector<char> chars(n * sizeof(T));
-
-  typedef
-  fcppt::container::raw_vector<T>
-  result_container;
-
-  if(!s.read(chars.data(),static_cast<std::streamsize>(chars.size())))
-    return fcppt::either::object<error_string,result_container>{_error};
-
-  result_container result(n);
-  for(std::size_t i = 0; i < chars.size();i += sizeof(T)) {
-    std::memcpy(&result[i/sizeof(T)],&chars[i],sizeof(T));
-    result[i/sizeof(T)] = fcppt::endianness::convert(result[i/sizeof(T)],_format);
-  }
-  return fcppt::either::object<error_string,result_container>{result};
-}
-
-namespace msl
-{
-struct level_actor
-{
-public:
-  typedef std::uint16_t id_type;
-  typedef std::uint16_t pos_type;
-private:
-  id_type id_;
-  pos_type pos_x_;
-  pos_type pos_y_;
-public:
-  level_actor(
-    id_type const _id,
-    pos_type const _pos_x,
-    pos_type const _pos_y)
-  :
-    id_{_id},
-    pos_x_{_pos_x},
-    pos_y_{_pos_y}
-  {}
-};
-
-struct level
-{
-public:
-  typedef
-  std::uint16_t
-  actor_container_length_type;
-
-  typedef
-  std::vector<level_actor>
-  actor_container;
-
-  typedef
-  std::array<std::uint8_t,480u>
-  unknown_container;
-
-  typedef
-  fcppt::container::raw_vector<std::uint16_t>
-  tile_container;
-
-  typedef
-  std::uint16_t
-  light_flag_type;
-
-  typedef
-  std::uint16_t
-  width_type;
-private:
-  std::string mask_tiles_;
-  std::string solid_tiles_;
-  std::string backdrop_;
-  std::string music_;
-  light_flag_type light_flags_;
-  actor_container actors_;
-  unknown_container unknown_;
-  tile_container tile_codes_;
-public:
-  level(
-    std::string const &_mask_tiles,
-    std::string const &_solid_tiles,
-    std::string const &_backdrop,
-    std::string const &,
-    std::string const &,
-    std::string const &_music,
-    std::string const &,
-    light_flag_type const _light_flags,
-    width_type const _width,
-    actor_container const &_actors,
-    unknown_container const &_unknown,
-    tile_container const &_tile_codes)
-  :
-    mask_tiles_{_mask_tiles},
-    solid_tiles_{_solid_tiles},
-    backdrop_{_backdrop},
-    music_{_music},
-    light_flags_{_light_flags},
-    actors_{_actors},
-    unknown_{_unknown},
-    tile_codes_{_tile_codes}
-  {}
-
-  std::string const &mask_tiles() const { return mask_tiles_; }
-  std::string const &solid_tiles() const { return solid_tiles_; }
-  std::string const &backdrop() const { return backdrop_; }
-  std::string const &music() const { return music_; }
-  light_flag_type light_flags() const { return light_flags_; }
-  actor_container const &actors() const { return actors_; }
-  unknown_container const &unknown() const { return unknown_; }
-  tile_container const &tile_codes() const { return tile_codes_; }
-};
-
-template<typename Type,typename Error>
-fcppt::either::object<Error,Type>
-read_to_either(
-	std::istream &_stream,
-	fcppt::endianness::format const _format,
-	Error const &_error) {
-  return
-    fcppt::either::from_optional(
-      fcppt::io::read<Type>(_stream,_format),
-      [&_error]() { return _error; });
-}
-
-fcppt::either::object<error_string,level::unknown_container>
-load_unknown(std::istream &s)
-{
-  level::unknown_container chars;
-  return
-    s.read(reinterpret_cast<char *>(chars.data()),chars.size())
-    ?
-      fcppt::either::object<error_string,level::unknown_container>{chars}
-    :
-      fcppt::either::object<error_string,level::unknown_container>{error_string{"unknown"}};
-
-}
-
-fcppt::either::object<error_string,level_actor>
-load_actor(std::istream &s)
-{
-  auto id =
-    read_to_either<level_actor::id_type>(
-      s,
-      fcppt::endianness::format::little,
-      error_string{"actor id"});
-  auto pos_x =
-    read_to_either<level_actor::pos_type>(
-      s,
-      fcppt::endianness::format::little,
-      error_string{"actor_pos_x"});
-  auto pos_y =
-    read_to_either<level_actor::pos_type>(
-      s,
-      fcppt::endianness::format::little,
-      error_string{"actor_pos_y"});
-  return
-    fcppt::either::apply(
-      [](
-	level_actor::id_type const _id,
-	level_actor::pos_type const _pos_x,
-	level_actor::pos_type const _pos_y)
-      {
-	return level_actor{_id,_pos_x,_pos_y};
-      },
-      id,
-      pos_x,
-      pos_y);
-}
-
-fcppt::either::object<error_string,level::actor_container>
-load_actors(std::istream &s)
-{
-  return
-      fcppt::either::bind(
-	read_to_either<level::actor_container_length_type>(
-	  s,
-	  fcppt::endianness::format::little,
-	  error_string{"actor_count"}),
-	[&s](level::actor_container_length_type const actor_count)
-	{
-	  return
-	    fcppt::either::cat<level::actor_container>(
-	      fcppt::algorithm::generate_n<std::vector<fcppt::either::object<error_string,level_actor>>>(
-		static_cast<std::size_t>(actor_count),
-		[&s]() { return load_actor(s); }));
-	});
-}
-
-fcppt::either::object<error_string,level>
-load_level(std::istream &s)
-{
-  auto mask_tiles = read_string<12>(s,"mask_tiles");
-  auto solid_tiles = read_string<12>(s,"solid_tiles");
-  auto backdrop = read_string<12>(s,"backdrop");
-  auto unused1 = read_string<12>(s,"unused 1");
-  auto unused2 = read_string<12>(s,"unused 2");
-  auto music = read_string<12>(s,"music");
-  auto unused3 = read_string<12>(s,"unused 3");
-  auto light_flags = read_to_either<level::light_flag_type>(s,fcppt::endianness::format::little,error_string{"light_flags"});
-  auto width = read_to_either<level::width_type>(s,fcppt::endianness::format::little,error_string{"width_flags"});
-  auto actors = load_actors(s);
-  auto unknown = load_unknown(s);
-  auto tiles = read_array<std::uint16_t>(s,16767u,fcppt::endianness::format::little,error_string{"tiles"});
-  return
-    fcppt::either::apply(
-    [](
-      std::string const &_mask_tiles,
-      std::string const &_solid_tiles,
-      std::string const &_backdrop,
-      std::string const &_unused1,
-      std::string const &_unused2,
-      std::string const &_music,
-      std::string const &_unused3,
-      level::light_flag_type const _light_flags,
-      level::width_type const _width,
-      level::actor_container const &_actors,
-      level::unknown_container const &_unknown,
-      level::tile_container const &_tiles) {
-	return level{_mask_tiles,_solid_tiles,_backdrop,_unused1,_unused2,_music,_unused3,_light_flags,_width,_actors,_unknown,_tiles};
-      },
-      mask_tiles,
-      solid_tiles,
-      backdrop,
-      unused1,
-      unused2,
-      music,
-      unused3,
-      light_flags,
-      width,
-      actors,
-      unknown,
-      tiles);
-}
+  _stream << '(';
+  for(T const &val : _array)
+    _stream << val << ',';
+  return _stream << ')';
 }
 }
 
 int main(
-  int const argc,
-  char const * const *argv)
+  int argc,
+  char **argv)
 {
-
+  if(argc != 2) {
+    std::cerr << "usage: " << argv[0] << " <level-file>\n";
+    return EXIT_FAILURE;
+  }
+  std::ifstream file_stream{argv[1]};
+  return
+    fcppt::optional::maybe(
+      libstryker::level::read(file_stream),
+      []{
+        std::cerr << "Reading failed\n";
+        return EXIT_FAILURE;
+      },
+      [](
+        libstryker::level::record const &_level
+      ){
+        std::cout << _level << '\n';
+        return EXIT_SUCCESS;
+      }
+    );
 }
