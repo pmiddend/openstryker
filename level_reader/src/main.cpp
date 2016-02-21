825f1620f21e22639e2cd8c6dc4a392e60329f10
Carl Philipp Reh <carlphilippreh@gmail.com>
2016-02-21 14:49:50 +0100

    Use fcppt::args in level_reader

--- level_reader/src/main.cpp
+++ level_reader/src/main.cpp
@@ -2,13 +2,18 @@
 #include <libstryker/level/record.hpp>
 #include <alda/raw/record_output.hpp>
 #include <majutsu/record_output.hpp>
+#include <fcppt/args.hpp>
+#include <fcppt/args_vector.hpp>
+#include <fcppt/reference.hpp>
 #include <fcppt/math/vector/output.hpp>
+#include <fcppt/container/at_optional.hpp>
 #include <fcppt/optional/maybe.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <array>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <string>
 #include <ostream>
 #include <vector>
 #include <fcppt/config/external_end.hpp>
@@ -33,27 +38,32 @@ std::ostream &operator<<(std::ostream &_stream,std::vector<T> const &_array)
 }
 }
 
-int main(
-  int argc,
-  char **argv)
+int main(int argc, char **argv)
 {
-  if(argc != 2) {
-    std::cerr << "usage: " << argv[0] << " <level-file>\n";
-    return EXIT_FAILURE;
-  }
-  std::ifstream file_stream{argv[1]};
+  fcppt::args_vector const args(fcppt::args(argc,argv));
   return
     fcppt::optional::maybe(
-      libstryker::level::read(file_stream),
+      fcppt::container::at_optional(args,1u),
       []{
-        std::cerr << "Reading failed\n";
+        std::cerr << "usage: level_reader <level-file>\n";
         return EXIT_FAILURE;
       },
-      [](
-        libstryker::level::record const &_level
-      ){
-        std::cout << _level << '\n';
-        return EXIT_SUCCESS;
-      }
-    );
+      [](fcppt::reference<std::string const> file_name)
+      {
+        std::ifstream file_stream{file_name.get()};
+        return
+          fcppt::optional::maybe(
+            libstryker::level::read(file_stream),
+            []{
+              std::cerr << "Reading failed\n";
+              return EXIT_FAILURE;
+            },
+            [](
+              libstryker::level::record const &_level
+            ){
+              std::cout << _level << '\n';
+              return EXIT_SUCCESS;
+            }
+          );
+      });
 }
